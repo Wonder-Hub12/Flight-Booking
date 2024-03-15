dfacb02d53e081da2d2981eef4351184fee12aa1
mahendra <153123561+mahendra8432@users.noreply.github.com>
2024-03-15 08:47:01 +0000

    UserState Modification

--- fly/src/Components/Navbar/Navbar.jsx
+++ fly/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {SiConsul} from 'react-icons/si'
 import {BsPhoneVibrate} from 'react-icons/bs'
 import {AiOutlineGlobal} from 'react-icons/ai'
 
 import logo from '../../assets/logo.png'
 const Navbar = () => {
+
+  // let us remove in small width
+  
+
+
+
+
+
+
   return (
     <div className='navBar flex'>
          <div className="navBarOne flex">
